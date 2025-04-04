e5de6b365e457d460bf1414cd166e802c0cbe4fd
claudiahalip <cvamesu@legalzoom.com>
2025-04-04 11:17:36 -0500

    fix-styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,26 +2,9 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center m-10">
-      <div className="flex">
-        <img
-          src="/images/AIC2.jpeg"
-          alt="Art Institute of Chicago 1"
-          width="800"
-        ></img>
-        <img
-          src="/images/AIC3.jpeg"
-          alt="Art Institute of Chicago 2"
-          width="800"
-        ></img>
-        <img
-          src="/images/AIC4.jpeg"
-          alt="Art Institute of Chicago 3"
-          width="800"
-        ></img>
-      </div>
-      <div className="flex flex-col gap-6 bg-orange-100 px-72 py-10">
-        <h1 className="text-3xl font-bold text-center mt-10 text-red-500">
+    <div className="flex flex-col items-center md:m-10">
+      <div className="flex flex-col gap-6 bg-orange-100 px-6 md:px-24 lg:px-72 py-10">
+        <h1 className="md:text-3xl text-md font-bold text-center mt-10 text-red-500">
           The art of Art Institute of Chicago
         </h1>
         <p>
@@ -40,27 +23,42 @@ export default function Home() {
         <Link
           to={`/all-art`}
           title="See art list"
-          className="flex rounded-2xl shadow-md shadow-slate-500 p-2 justify-center w-32 text-red-500 cursor-pointer hover:shadow-lg hover:shadow-slate-600"
+          className="flex rounded-2xl shadow-md shadow-slate-500 p-2 justify-center w-32 text-red-500 cursor-pointer hover:shadow-lg hover:shadow-slate-600 bg-white"
         >
           See art list
         </Link>
       </div>
-      <div className="flex">
+      <div className="flex flex-wrap justify-center w-full">
         <img
           src="/images/AIC1.jpeg"
           alt="Art Institute of Chicago 4"
-          width="800"
+          className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 object-cover lg:basis-1/3 "
         ></img>
 
         <img
           src="/images/AIC5.jpeg"
           alt="Art Institute of Chicago 5"
-          width="800"
+          className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 object-cover lg:basis-1/3 "
         ></img>
         <img
           src="/images/AIC6.jpeg"
           alt="Art Institute of Chicago 6"
-          width="800"
+          className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 object-cover lg:basis-1/3 "
+        ></img>
+        <img
+          src="/images/AIC2.jpeg"
+          alt="Art Institute of Chicago 1"
+          className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 object-cover lg:basis-1/3 "
+        ></img>
+        <img
+          src="/images/AIC3.jpeg"
+          alt="Art Institute of Chicago 2"
+          className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 object-cover lg:basis-1/3 "
+        ></img>
+        <img
+          src="/images/AIC4.jpeg"
+          alt="Art Institute of Chicago 3"
+          className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 object-cover lg:basis-1/3 "
         ></img>
       </div>
     </div>
